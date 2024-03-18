426a12261316010f25f6d42d225529ff625cb557
lesmadeit <lesmwendwa@gmail.com>
2024-03-18 15:23:40 +0300

    template changes

--- blog-main/blog/blog/settings.py
+++ blog-main/blog/blog/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,6 +133,8 @@ REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static/')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 
